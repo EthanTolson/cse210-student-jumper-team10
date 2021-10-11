ccba04e51ad2e2de82d25e4169b97cc8858aff9b
Ethan Tolson <tol18009@byui.edu>
2021-10-11 13:11:13 -0700

    Added fix for possible bug- not sure if it will ever activate.

--- jumper/game/secretword.py
+++ jumper/game/secretword.py
@@ -4,11 +4,15 @@ class Secretword:
     def __init__(self):
 
 
-        self.s_word = ""
+        self.s_word = "ERROR"
+
         with open("jumper/game/wordss.txt", "r") as file:
             words = file.readlines()
             self.s_word = words[random.randint(0,9509)]
+
         self.s_word = self.s_word.strip()
+        if(self.s_word == "ERROR"):
+            print("There is an issue with the program. Program should still run properly. ERROR with file.")
         print(f"Secret Word has been chosen! (¬‿ ¬) {self.s_word}")
 
 
