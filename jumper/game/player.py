80b4599d2469d9b138bba5c4c667d63d8d28276e
Ethan Tolson <tol18009@byui.edu>
2021-10-11 14:09:59 -0700

    Added bare comments to each class as well as added methods for each class. Worked a bit on secretword init and get turns method in player Copied console class from hide and seek code.

--- jumper/game/player.py
+++ jumper/game/player.py
@@ -1,5 +1,58 @@
 
 
 class Player:
+    """A code template for the player who guesses 
+    letters in a word. The responsibility of this class is to 
+    hold guesses as players attempt to guess the word.
+
+    Stereotype:
+        Info Holder
+
+    Attributes:
+        i_guesses_made (list): list of letters; updates as user inputs incorrect values
+        guesses_left (integer): number of guesses until player fails
+    
+    
+    """
     def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
+
+        Args:
+            self (Player): An instance of Player.
+        """
+        self.guesses_left = 8
+        self.i_guesses_made = ["", "", "", "", "", "", "", ""]
         pass
+
+    def get_graphic(self):
+        """Returns a string that contains image to show player
+        
+        Args: 
+            self (Player): An instance of Player.
+        """
+        pass
+
+    def can_pick(self, guess):
+        """Returns a boolean value if the player can input the desired value
+        Example: User cannot input numbers/special characters or a value 
+        they have already input
+        
+        Args:
+            self (Player): An instance of Player.
+            guess (character): a user inputted variable
+        """
+        
+        pass
+        
+    def get_turns(self, valid_play):
+        """Gives the number of turns remaining and updates when player uses one
+
+        Args:
+            self (Player): An instance of Player.
+            valid_play (boolean): passes true if player has guessed a correct value
+        """
+        turns = f"You have {self.guesses_left} guesses remaining"
+        if(not valid_play):
+            self.guesses_left = self.guesses_left - 1
+
+        return turns
--- jumper/game/secretword.py
+++ jumper/game/secretword.py
@@ -1,20 +1,57 @@
 import random
 
 class Secretword:
+    """A code template for the secret word that the 
+    computer chooses for each game. The responsibility 
+    of this class is to get a secret word for the user to guess.
+    
+    Stereotype:
+        Info Holder
+
+    Attributes:
+        s.word (list): list of letters that make up secret word
+        s_word_revealed (list): list of charaters that make 
+            up non guessed and guessed letters of secret word
+    """
     def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
 
+        Args:
+            self (Secretword): an instance of Secretword
+        """
 
-        self.s_word = "ERROR"
+        self.s_word = "ER"
 
         with open("jumper/game/wordss.txt", "r") as file:
             words = file.readlines()
             self.s_word = words[random.randint(0,9509)]
 
         self.s_word = self.s_word.strip()
-        if(self.s_word == "ERROR"):
+        if(self.s_word == "ER"):
             print("There is an issue with the program. Program should still run properly. ERROR with file.")
-        print(f"Secret Word has been chosen! (¬‿ ¬) {self.s_word}")
-
-
+        print(f"Secret Word has been chosen! (¬‿ ¬)\n")
+        self.s_word_revealed = []
+        for value in self.s_word:
+            self.s_word_revealed.append("_")
         
         pass
+
+    def check_guess(self, guess):
+        """Returns true if guess is in secret word
+        Args:
+            self (Secretword): an instance of Secretword
+            guess (charater): user inputted guess
+        """
+        pass
+
+    def secret_word_revealer(self, guess = "!"):
+        """Returns a message with the correct guesses revealed. 
+        If guess is included then update self.s_word_revealed
+        Ex.        _ _ A _ _
+        
+        Args:
+            self
+            guess (character): user inputted character
+        """
+        pass
+    
