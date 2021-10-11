bec8fd6ea40ef0db1b569bb24b877b951ac6c4e7
Ethan Tolson <tol18009@byui.edu>
2021-10-11 13:01:18 -0700

    Added all class files, created barebones for all classes, added a text file with list of random words all greater than 3 characters long. added name to readme.

---
+++ jumper/game/secretword.py
@@ -0,0 +1,16 @@
+import random
+
+class Secretword:
+    def __init__(self):
+
+
+        self.s_word = ""
+        with open("game/wordss.txt", "r") as file:
+            words = file.readlines()
+            self.s_word = words[random.randint(0,9509)]
+        self.s_word = self.s_word.strip()
+        print(f"Secret Word has been chosen! (¬‿ ¬) {self.s_word}")
+
+
+        
+        pass
