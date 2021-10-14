f781d01800d9887e7c88cf9552a4dd6c87bb054c
Ethan Tolson <tol18009@byui.edu>
2021-10-14 14:40:20 -0700

    Fixed

--- jumper/game/player.py
+++ jumper/game/player.py
@@ -62,7 +62,7 @@ class Player:
         #concatenate full_jumper
         for i in range(len(jumper)):
 
-            if i <= self.guesses_left:
+            if i >= self.guesses_left:
                 full_jumper = f"{full_jumper}\n{jumper_alt[i]}"
             else:
                 full_jumper = f"{full_jumper}\n{jumper[i]}"
